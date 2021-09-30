8dcc9b52dd5a615c34e934951b07976848775574
cyoung90 <chayoung.dev@gmail.com>
2021-09-30 15:54:40 +0900

    0.1 ~ 0.4 강의

---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "cyoung90",
+  age = 31,
+  gender = "male";
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
